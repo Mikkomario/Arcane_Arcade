7cead7fbec6accb3eb87d867b08a5518124e62a1
Mikkomario <mikko_hilpinen@hotmail.com>
2013-08-29 01:13:10 +0300

    Created two subclasses for the spelleffectcreator class, each extending the functionalities of the original class

---
+++ src/arcane_arcade_spelleffects/FollowerSpellEffectCreator.java
@@ -0,0 +1,64 @@
+package arcane_arcade_spelleffects;
+
+import drawnobjects.DrawnObject;
+import handlers.ActorHandler;
+import worlds.Room;
+
+/**
+ * This class works like a spelleffectcreator except that it follows a specified 
+ * object around, keeping track of its position and speed
+ *
+ * @author Mikko Hilpinen.
+ *         Created 29.8.2013.
+ */
+public abstract class FollowerSpellEffectCreator extends SpellEffectCreator
+{
+	// ATTRIBUTES	-----------------------------------------------------
+	
+	private DrawnObject followedobject;
+	
+	
+	// CONSTRUCTOR	-----------------------------------------------------
+	
+	/**
+	 * Creates a new followerspelleffectcreator that follows the specified object.
+	 *
+	 * @param duration How long the creator remains active
+	 * @param creationdelay How long interval is kept between object creations 
+	 * (in steps)
+	 * @param burstsize How many objects are created at once
+	 * @param actorhandler The actorhandler that will inform the creator about 
+	 * steps
+	 * @param room The room the creator is created into
+	 * @param followedobject The object the creator keeps track of
+	 */
+	public FollowerSpellEffectCreator(int duration, int creationdelay,
+			int burstsize, ActorHandler actorhandler, Room room, 
+			DrawnObject followedobject)
+	{
+		super(duration, creationdelay, burstsize, actorhandler, room);
+		
+		this.followedobject = followedobject;
+	}
+	
+	
+	// GETTERS & SETTERS	---------------------------------------------
+	
+	/**
+	 * @return The object the creator keeps track of
+	 */
+	protected DrawnObject getFollowedObject()
+	{
+		return this.followedobject;
+	}
+	
+	/**
+	 * Changes the object the creator follows
+	 *
+	 * @param newfollowed The new followed object
+	 */
+	protected void setFollowedObject(DrawnObject newfollowed)
+	{
+		this.followedobject = newfollowed;
+	}
+}
---
+++ src/arcane_arcade_spelleffects/MovingSpellEffectCreator.java
@@ -0,0 +1,119 @@
+package arcane_arcade_spelleffects;
+
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+
+import drawnobjects.BasicPhysicDrawnObject;
+import handleds.Collidable;
+import handlers.ActorHandler;
+import helpAndEnums.CollisionType;
+import helpAndEnums.DepthConstants;
+import helpAndEnums.DoublePoint;
+import helpAndEnums.Movement;
+import worlds.Room;
+
+/**
+ * Movingspelleffectcreator moves to a certain direction with a certain speed 
+ * and friction while creating spell effects
+ *
+ * @author Mikko Hilpinen.
+ *         Created 29.8.2013.
+ */
+public abstract class MovingSpellEffectCreator extends FollowerSpellEffectCreator
+{
+	// CONSTRUCTOR	-----------------------------------------------------
+	
+	/**
+	 * Creates a new creator to the given position moving with the given 
+	 * movement and friction
+	 *
+	 * @param duration How long will the creator live (steps)
+	 * @param creationdelay How many steps there will be between each creation
+	 * @param burstsize How many spelleffects are created at once
+	 * @param actorhandler The actorhandler that informs the object about steps
+	 * @param room The room in which the creator is created
+	 * @param x The creator's x-coordinate
+	 * @param y The creator's y-coordinate
+	 * @param initialmovement The movement the creator has when its created
+	 * @param friction The friction the creator has (speed / step)
+	 */
+	public MovingSpellEffectCreator(int duration, int creationdelay,
+			int burstsize, ActorHandler actorhandler, Room room, 
+			int x, int y, Movement initialmovement, double friction)
+	{
+		super(duration, creationdelay, burstsize, actorhandler, room, null);
+		setFollowedObject(new FollowedPhysicObject(x, y, initialmovement, 
+				friction, actorhandler));
+	}
+	
+	
+	// SUBCLASSES	------------------------------------------------------
+	
+	private class FollowedPhysicObject  extends BasicPhysicDrawnObject
+	{
+		// CONSTRUCTOR	--------------------------------------------------
+		
+		/**
+		 * Creates a new followedphysicobject to the given position added 
+		 * to the given handlers
+		 *
+		 * @param x The object's x-coordinate
+		 * @param y The object's y-coordinate
+		 * @param initialmovement What kind of movement the object has at 
+		 * the beginning
+		 * @param friction How much friction the object has 
+		 * @param actorhandler The actorhandler that will call the object's 
+		 * act event
+		 */
+		public FollowedPhysicObject(int x, int y, Movement initialmovement, 
+				double friction, ActorHandler actorhandler)
+		{
+			super(x, y, DepthConstants.NORMAL, false, CollisionType.BOX, null, 
+					null, null, actorhandler);
+			
+			// Sets movement & friction
+			setMovement(initialmovement);
+			setFriction(friction);
+		}
+		
+		
+		// IMPLEMENTED METHODS	-----------------------------------------
+
+		@Override
+		public void onCollision(ArrayList<DoublePoint> colpoints,
+				Collidable collided)
+		{
+			// Doesn't react to collisions
+		}
+
+		@Override
+		public int getWidth()
+		{
+			return 0;
+		}
+
+		@Override
+		public int getHeight()
+		{
+			return 0;
+		}
+
+		@Override
+		public int getOriginX()
+		{
+			return 0;
+		}
+
+		@Override
+		public int getOriginY()
+		{
+			return 0;
+		}
+
+		@Override
+		public void drawSelfBasic(Graphics2D g2d)
+		{
+			// Doesn't draw itself
+		}
+	}
+}
