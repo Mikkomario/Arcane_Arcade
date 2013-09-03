716294d0673f55a0eb0868bcc79a18a113e43fa5
Mikkomario <mikko_hilpinen@hotmail.com>
2013-09-03 17:30:39 +0300

    Added a new room class that uses settings and also made an interface for all the roomcreators and made it support settings as well.

---
+++ src/arcane_arcade_worlds/RoomObjectCreator.java
@@ -0,0 +1,22 @@
+package arcane_arcade_worlds;
+
+import listeners.RoomListener;
+
+/**
+ * RoomObjectCreator creates a bunch of objects at the start of the room 
+ * using different settings as help.
+ *
+ * @author Mikko Hilpinen.
+ *         Created 3.9.2013.
+ */
+public interface RoomObjectCreator extends RoomListener
+{
+	/**
+	 * Changes the settings the creator uses when it creates the objects in 
+	 * the room
+	 *
+	 * @param setting The setting object that tells information about how to 
+	 * create the objects
+	 */
+	public void setSettings(AreaSetting setting);
+}
---
+++ src/arcane_arcade_worlds/SettingUsingRoom.java
@@ -0,0 +1,66 @@
+package arcane_arcade_worlds;
+
+import java.util.ArrayList;
+
+import backgrounds.Background;
+
+import worlds.Room;
+
+/**
+ * SettingUsingRoom allows the use of settings and objectcreators and can 
+ * inform the objectcreator about new settings when necessary
+ *
+ * @author Mikko Hilpinen.
+ *         Created 3.9.2013.
+ */
+public class SettingUsingRoom extends Room
+{
+	// ATTRIBUTES	------------------------------------------------------
+	
+	private RoomObjectCreator objectcreator;
+	
+	
+	// CONSTRUCTOR	-------------------------------------------------------
+	
+	/**
+	 * Creates a new room that uses the given objectcreator to create the 
+	 * objects in the room
+	 *
+	 * @param objectcreator The objectcreator that will create the objects in 
+	 * the room
+	 * @param backgrounds The backgrounds the room uses
+	 */
+	public SettingUsingRoom(RoomObjectCreator objectcreator, 
+			ArrayList<Background> backgrounds)
+	{
+		super(backgrounds);
+		
+		// Initializes attributes
+		this.objectcreator = objectcreator;
+	}
+	
+	
+	// OTHER METHODS	---------------------------------------------------
+	
+	/**
+	 * Changes the settings the objectcreator uses to create the objects
+	 *
+	 * @param setting The new settings used by the objectcreator
+	 */
+	public void setSettings(AreaSetting setting)
+	{
+		this.objectcreator.setSettings(setting);
+	}
+	
+	/**
+	 * Starts the room using the given settings
+	 *
+	 * @param setting The setings that affect how the objects in the room are 
+	 * created
+	 */
+	public void start(AreaSetting setting)
+	{
+		setSettings(setting);
+		start();
+	}
+}
